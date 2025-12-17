f83adec9a26a999310dfae08e177b718ff847405
Yashas Shetty <yashass825@gmail.com>
2025-12-17 18:42:08 +0530

    basic app

--- app/main.py
+++ app/main.py
@@ -22,3 +22,11 @@ def create_item(item: Item):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    app = FastAPI()
+
+    @app.get("/")
+    def read_root():
+        return {"Hello": "World"}
+
+    if __name__ == "__main__":
+        uvicorn.run(app, host="0.0.0.0", port=8000)
